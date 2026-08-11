bceb9744355c2a0a4334e5a98532e48b644e77f0
Het Mevada <hetmevada37@gmail.com>
2026-08-11 17:30:18 +0530

    Fix WhatsApp button mobile positiion

--- src/components/common/WhatsAppButton.jsx
+++ src/components/common/WhatsAppButton.jsx
@@ -4,7 +4,23 @@ function WhatsAppButton() {
       href="https://wa.me/918160815662?text=Hello%20Kanha%20Restaurant,%20I%20want%20to%20book%20a%20table."
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-6 right-6 bg-green-500 text-white w-16 h-16 rounded-full flex items-center justify-center shadow-2xl hover:bg-green-600 transition z-50 text-3xl"
+      className="
+        fixed
+        bottom-5 right-5
+        md:bottom-6 md:right-6
+        bg-green-500
+        text-white
+        w-14 h-14
+        md:w-16 md:h-16
+        rounded-full
+        flex items-center justify-center
+        shadow-2xl
+        hover:bg-green-600
+        hover:scale-110
+        transition-all duration-300
+        z-50
+        text-2xl md:text-3xl
+      "
     >
       💬
     </a>
